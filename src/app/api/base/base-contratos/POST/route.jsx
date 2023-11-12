b1e3dd462db125b5a33f4336d3388d3247c25e07
GEdO23 <rm99632@fiap.com.br>
2023-11-12 19:45:25 -0300

    post

--- src/app/api/base/base-contratos/POST/route.jsx
+++ src/app/api/base/base-contratos/POST/route.jsx
@@ -1,22 +1,105 @@
+import { promises as fs } from "fs";
 import { NextResponse } from "next/server";
-import {promises as fs} from "fs";
-
-export async function POST(request, {params}) {
-
-    // Obtendo o request json do usuario
-    const userRequest = await request.json();
-    
-    // Obtendo a URL da API de Java
-    const apiURL = 'http://localhost:8080/portoapi/webapi/bicicletas';
-
-    // Coletando o arquivo
-    const file = await fs.readFile(apiURL, 'utf-8');
-
-    // Extrair a lista de bicicletas do arquivo json
-    const listaBicicletas = await JSON.parse(file);
 
 
+//     {
+//       "id": 3,
+//       "marca": "Specialized",
+//       "modelo": "Rockhopper Expert",
+//       "numero_serie": "S456789123",
+//       "data_compra": "05/03/2023",
+//       "preco_compra": 5999,
+//       "nota_fiscal": "nota_fiscal_3.pdf",
+//       "img": ""
+//     },
+//     {
+//       "id": 4,
+//       "marca": "Trek",
+//       "modelo": "Marlin 7",
+//       "numero_serie": "T123456789",
+//       "data_compra": "01/04/2023",
+//       "preco_compra": 4499,
+//       "nota_fiscal": "nota_fiscal_4.pdf",
+//       "img": ""
+//     },
+//     {
+//       "id": 5,
+//       "marca": "Cannondale",
+//       "modelo": "Trail SL 1",
+//       "numero_serie": "C987654321",
+//       "data_compra": "30/04/2023",
+//       "preco_compra": 7999,
+//       "nota_fiscal": "nota_fiscal_5.pdf",
+//       "img": ""
+//     },
+//     {
+//       "id": 6,
+//       "marca": "Giant",
+//       "modelo": "Talon 1",
+//       "numero_serie": "G456789123",
+//       "data_compra": "15/05/2023",
+//       "preco_compra": 5499,
+//       "nota_fiscal": "nota_fiscal_6.pdf",
+//       "img": ""
+//     },
+//     {
+//       "id": 7,
+//       "marca": "Fuji",
+//       "modelo": "Pulse 1",
+//       "numero_serie": "F123456789",
+//       "data_compra": "01/06/2023",
+//       "preco_compra": 5999,
+//       "nota_fiscal": "nota_fiscal_7.pdf",
+//       "img": ""
+//     },
+//     {
+//       "id": 8,
+//       "marca": "Scott",
+//       "modelo": "Outlander XTR",
+//       "numero_serie": "SC123456789",
+//       "data_compra": "01/07/2023",
+//       "preco_compra": 5499,
+//       "nota_fiscal": "nota_fiscal_8.pdf",
+//       "img": ""
+//     },
+//     {
+//       "id": 9,
+//       "marca": "Specialized",
+//       "modelo": "Liv Pro FX 2",
+//       "numero_serie": "SP123456789",
+//       "data_compra": "01/08/2023",
+//       "preco_compra": 5999,
+//       "nota_fiscal": "nota_fiscal_9.pdf",
+//       "img": ""
+//     },
+//     {
+//       "id": 10,
+//       "marca": "Santa Cruz",
+//       "modelo": "Supernova 3",
+//       "numero_serie": "SN123456789",
+//       "data_compra": "01/09/2023",
+//       "preco_compra": 5499,
+//       "nota_fiscal": "nota_fiscal_10.pdf",
+//       "img": ""
+//     }
 
+export async function POST(request, response) {
 
-    return NextResponse.json();
+  const userRequest = await request.json();
+
+  const javaAPIURL = process.cwd() + "/src/app/api/base/db.json";
+  const file = await fs.readFile(javaAPIURL, "utf-8");
+
+  const lista = await JSON.parse(file);
+  const listaContratos = await lista.seguros_bike;
+
+  const newId = await listaContratos[listaContratos.length - 1].id + 1;
+
+  userRequest.id = await newId;
+
+  await listaContratos.push(userRequest);
+
+  await fs.writeFile(javaAPIURL, JSON.stringify(lista));
+
+  return NextResponse.json(userRequest);
 }

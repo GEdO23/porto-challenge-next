a94a726a4483694fdee2a4a5f05cab7e27cf645c
GEdO23 <rm99632@fiap.com.br>
2023-11-12 20:45:37 -0300

    HandleChange de formulario.

--- src/app/seguros/add/page.jsx
+++ src/app/seguros/add/page.jsx
@@ -1,74 +1,127 @@
+"use client";
+
 import Image from "next/image";
 import imgIco from "/public/IconeImagem.svg";
+import { useState } from "react";
 
-export default function SegurosAdd() {
+export default function CadSeguro() {
+    const [contrato, setContrato] = useState({
+        marca: "",
+        modelo: "",
+        numero_serie: "",
+        data_compra: "",
+        preco_compra: "",
+        nota_fiscal: "",
+        img: "",
+    });
+
+    const handleChange = (e)=> {
+        const {name, value} = e.target;
+
+        setContrato({...contrato, [name]: value});
+    };
+
+
 
     return (
-        // "id": 1,
-        // "marca": "Caloi",
-        // "modelo": "Elite Carbon",
-        // "numero_serie": "C123456789",
-        // "data_compra": "15/01/2023",
-        // "preco_compra": 9999.99,
-        // "nota_fiscal": "nota_fiscal_1.pdf",
-        // "img": ""
         <div id="main">
-            <h1 className='text-center'>Cadastro de Seguro</h1>
+            <h1 className="text-center">Cadastro de Seguro</h1>
             <div>
                 <form>
+                <div>
                     <div>
-                        <div>
-                            <label htmlFor="idMarca">
-                                Marca:
-                                <input type="text" id="idMarca"/>
-                            </label>
-
-                        </div>
-                        <div>
-                            <label htmlFor="idModelo">
-                                Modelo:
-                                <input type="text" id="idModelo"/>
-                            </label>
-
-                        </div>
-                        <div>
-                            <label htmlFor="idNumserie">
-                                Número de Série:
-                                <input type="text" id="idNumserie"/>
-                            </label>
-
-                        </div>
-                        <div>
-                            <label htmlFor="idDtCompra">
-                                Data da Compra:
-                                <input type="date" id="idDtCompra"/>
-                            </label>
-
-                        </div>
-                        <div>
-                            <label htmlFor="idDtPreco">
-                                Preço:
-                                <input type="number" id="idDtPreco"/>
-                            </label>
-
-                        </div>
-                        <div>
-                            <label htmlFor="idNotafiscal">
-                                Nota Fiscal:
-                                <input type="file" id="idNotafiscal"/>
-                            </label>
-
-                        </div>
-                        <div>
-                            <label htmlFor="idFotoBike">
-                                Foto da Bike:
-                                <input type="text" id="idFotoBike"/>
-                            </label>
-
-                        </div>
+                        <label htmlFor="idMarca">
+                            Marca:
+                            <input 
+                            type="text"
+                            name="marca"
+                            id="idMarca"
+                            placeholder="Digite a marca de sua bike."
+                            value={contrato.marca}
+                            onChange={handleChange}
+                            />
+                        </label>
                     </div>
+                    <div>
+                        <label htmlFor="idModelo">
+                            Modelo:
+                            <input 
+                            type="text" 
+                            name="modelo"
+                            id="idModelo"
+                            placeholder="Digite o modelo de sua bike."
+                            value={contrato.modelo} 
+                            onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label htmlFor="idNumserie">
+                            Número de Série:
+                            <input 
+                            type="text" 
+                            name="numero_serie"
+                            id="idNumserie"
+                            placeholder="Digite o número de série de sua bike."
+                            value={contrato.numero_serie}
+                            onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label htmlFor="idDtCompra">
+                            Data da Compra:
+                            <input 
+                            type="date" 
+                            name="data_compra"
+                            id="idDtCompra"
+                            value={contrato.data_compra}
+                            onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label htmlFor="idDtPreco">
+                            Preço:
+                            <input 
+                            type="number" 
+                            name="data_preco"
+                            id="idDtPreco"
+                            placeholder="Digite o valor da bike em sua compra."
+                            value={contrato.preco_compra}
+                            onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label htmlFor="idNotafiscal">
+                            Nota Fiscal:
+                            <input 
+                            type="file" 
+                            name="nota_fiscal"
+                            id="idNotafiscal"
+                            placeholder="Selecione a nota fiscal de sua bike."
+                            value={contrato.nota_fiscal}
+                            onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label htmlFor="idFotoBike">
+                            Foto da Bike:
+                            <input 
+                            type="text" 
+                            name="img"
+                            id="idFotoBike"
+                            placeholder="Informe a URL de uma foto de sua bike."
+                            value={contrato.img}
+                            onChange={handleChange}
+                            />
+                        </label>
+                    </div>
+                </div>
                 </form>
             </div>
         </div>
-    )
+    );
 }

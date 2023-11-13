c54fb33419d16d3e16c6103cf4d307899b1c562e
GEdO23 <rm99632@fiap.com.br>
2023-11-13 00:33:55 -0300

    Adcionando estilizando de contratos e corrigindo classes

--- src/app/seguros/page.jsx
+++ src/app/seguros/page.jsx
@@ -25,40 +25,31 @@ export default async function SegurosView() {
     }
 
     return (
-        <div className="flex flex-col mx-auto my-0 w-max mb-10" id="main">
-        <div>
-            <ul className="mx-auto my-0 w-max">
-            {seguros.map((contrato) => (
-                <li
-                key={contrato.id}
-                className="flex items-center max-w-screen-xl justify-between mb-10 gap-24"
-                >
-                <div className="flex gap-5">
-                    {/* <Image className="rounded-lg" width="100" height="50" src={contrato.img ? contrato.img : imgIco} alt="Foto da Bike" /> */}
-                    <div className="flex flex-col gap-2">
-                    <p className="text-black">
-                        {contrato.marca} - {contrato.modelo} - {contrato.numero_serie}
-                    </p>
-                    <p>
-                        {contrato.data_compra} - R$ {(Math.round(contrato.preco_compra * 100) / 100).toFixed(2)}
-                    </p>
-                    <p className="text-slate-600">{contrato.nota_fiscal}</p>
-                    </div>
-                </div>
+        <main className="contratos-container">
+            <ul className="contratos-lista">
+                {seguros.map((contrato) => (
+                    <li key={contrato.id} className="contrato-item">
+                        <div className="item-info">
+                            <Image className="item-imagem" width="100" height="50" src={contrato.img ? contrato.img : imgIco} alt="Foto da Bike" />
+                            <div className="text-container">
+                                <p>{contrato.marca} - {contrato.modelo} - {contrato.numero_serie}</p>
+                                <p>{contrato.data_compra} - R$ {(Math.round(contrato.preco_compra * 100) / 100).toFixed(2)}</p>
+                                <p className="light-text">{contrato.nota_fiscal}</p>
+                            </div>
+                        </div>
 
-                <div className="flex gap-5">
-                    <button className="btn btn-blue">Atualizar</button>
-                    <button className="btn btn-red" onClick={()=> handleDelete(contrato.id)}>Cancelar</button>
-                </div>
-                </li>
-            ))}
+                        <div className="button-container">
+                            <button type="button" className="btn btn-blue">Atualizar</button>
+                            <button type="button" className="btn btn-red" onClick={()=> handleDelete(contrato.id)}>Cancelar</button>
+                        </div>
+                    </li>
+                ))}
             </ul>
-        </div>
-        <div className="w-full flex justify-center">
-            <Link href="http://localhost:3000/seguros/add">
-                <button className="btn btn-blue rounded-full text-2xl">+</button>
-            </Link>
-        </div>
-        </div>
+            <div className="add-button-container">
+                <Link href="http://localhost:3000/seguros/add">
+                    <button className="btn btn-blue rounded-full text-2xl">+</button>
+                </Link>
+            </div>
+        </main>
     );
 }

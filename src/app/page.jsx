a492169c054e768a0bdd973f7970dc7e5faf4fd7
GEdO23 <rm99632@fiap.com.br>
2023-11-13 12:29:37 -0300

    Inicio de slider

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,38 @@
+import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+
+  const resposta = await fetch("http://localhost:3000/api/base/base-slider/GET/0");
+  const slider = await resposta.json();
+
   return (
     <>
     <main>
-      <h1>HOME</h1>
-      <h2>Pagina Principal</h2>
-      <Link href="/seguros">Contratos Seguro de Bike</Link>
+      <h1 className=" text-4xl">HOME</h1>
+      <h2 className=" text-2xl">Pagina Principal</h2>
+      <Link href="/seguros" className=" text-blue-600 underline">Contratos Seguro de Bike</Link>
+
+
+      <h2 className=" text-2xl">Imagens</h2>
+      <ul>
+        {
+          slider.map((slide)=> (
+            <li key={slide.id}>
+              <Image
+                src={slide.img.src}
+                alt={slide.img.alt}
+                layout="fill"
+                objectFit="cover"
+                className=" -z-10"
+              />
+              <h3 className=" text-red-600 text-xl">{slide.info.tit}</h3>
+              <p>{slide.info.desc}</p>
+
+            </li>
+          ))
+        }
+      </ul>
       
     </main>
     </>

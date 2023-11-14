830b50f470cd8695ce11d9449868ec2097b017b5
GEdO23 <rm99632@fiap.com.br>
2023-11-14 09:28:37 -0300

    Adcionando componentes de botão de manipulação de dados + movendo estilização especifica para a página + adcionando comentários explicativos para página de contratos.

---
+++ src/components/Button/Remove/ButtonRemove.jsx
@@ -0,0 +1,7 @@
+import { AiOutlineDelete } from "react-icons/ai";
+
+export default function ButtonRemove() {
+    return (
+        <AiOutlineDelete className=" fill-red-600 w-[25px] h-[25px] cursor-pointer" />
+    );
+}
